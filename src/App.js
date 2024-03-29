b1bb5f201c677457b0ce1b391163261c495ef2b4
Slowl13 <125129756+Slowl13@users.noreply.github.com>
2024-03-30 00:53:38 +0400

    today learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import Children from "./Children"
+import { useEffect, useState } from "react"
 
-export default function App({children}){
+export default function App(){
+    useEffect(() => {
+        localStorage.getItem("arr") ?? localStorage.setItem("arr", JSON.stringify([]));
+    },[])
+
+    
+    function setLocal(){
+        try{
+            var tempArr = [...JSON.parse(localStorage.getItem("arr"))]
+        }
+        catch(e){
+            tempArr = [];
+        }
+        
+        tempArr.push(val)
+        localStorage.setItem("arr", JSON.stringify(tempArr));
+        setList(tempArr);
+    }
+
+    function clearHistory(){
+        localStorage.setItem("arr", JSON.stringify([]))
+        setList(JSON.parse(localStorage.getItem("arr")))
+    }
+
+    const handlePress = e => {
+        if(e.key === 'Enter') { 
+         setLocal();
+        }
+    }
+
+    function filterHistory(){
+        console.log(list.sort((a,b) => a.length > b.length ? 1 : -1))
+        setList(list.filter(elem => true));
+    }
+
+    const [val, setVal] = useState("Begin")
+    const [list, setList] = useState(JSON.parse(localStorage.getItem("arr")))
+
     return (
         <>
-            <p>TEST BELOW</p>
-            {children}
-            <p>TEST UNDER</p>
+            <input value={val} onChange={(e) => setVal(prev => e.target.value)} onKeyDown={handlePress}></input>
+            <button onClick={() => setLocal()}>Test</button>
+            <p>{val}</p>
+            <h1>History</h1>
+            <button onClick={() => clearHistory()}>Clear History</button>
+            <button onClick={() => filterHistory()}>Filter by lenght</button>
+            <ul>
+            {
+                list.map(elem => <li>{elem}</li>)
+            }
+            </ul>
         </>
     )
 }
